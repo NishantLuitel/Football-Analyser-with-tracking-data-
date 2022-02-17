be20daaf1cbfd7bfd3f2f07f6d9d07c507b147ef
Amrit <baralamrit7@gmail.com>
2022-02-17 22:07:09 +0545

    added createVertex and createEdges methods

--- graph.py
+++ graph.py
@@ -4,42 +4,23 @@ Created on Wed Feb  9 14:03:51 2022
 
 @author: hp
 """
-
-class player:
+from Player import player
+from Edge import edge
+        
+class graph():
     
-    def __init__(self,x,y,num):
-        self.x=x
-        self.y=y
-        self.num =num
     
-    def getdetail(self):
-        print("the player is {}".format(self.num))
-    
-
-class edge(player):
-    
-    def __init__(self,fro,to,volume):
+    def __init__(self):
+        self.__players={}
+        self.__edges=set()
+        
+        # self.player1=player1;
+        # self.player2=player2;
+        # self.edge1=edge1;
+    def addedge(self,fro,to,volume):
         self.volume=volume
-        
         self.fro=fro
-        self.to=to
-        #player.__init__(self,fro,to)
-    def addegdge(self,fro,to,volume):
-        pass
-        
-    
-    # def defit(self):
-    #     print("the edge is from {} to {}".format(self.fro.num,self.to.num))
-        
-        
-class graph(edge,player):
-    
-    
-    def __init__(self,player1,player2,edge1):
-        self.player1=player1;
-        self.player2=player2;
-        self.edge1=edge1;
-    
+        self.to=to    
     # def addedge(self,player1,player2,vol)
     
     def plot(self):
@@ -47,11 +28,17 @@ class graph(edge,player):
 
 p1=player(0,0,1)
 p2=player(0,0,2)
-e1=edge(p1,p2,4)
+e1=edge(p1,p2,44)
 #e1.defit()
 g1=graph(p1,p2,e1)
 g1.plot()
 g=graph(p1,p2,edge)
+p3=player()
+p3.addplayer(0,0,3)
+p3.getdetail()
+e2=edge()
+e2.addedge(p1,p3,33)
+e2.display()
 
 # graph g(p1,p2,e1)
 # g.plot

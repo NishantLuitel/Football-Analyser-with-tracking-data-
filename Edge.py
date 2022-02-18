0ebb4284bea2be0b5f51312b54a05f176d0e500c
Amrit <baralamrit7@gmail.com>
2022-02-19 00:12:45 +0545

    sagar

--- Edge.py
+++ Edge.py
@@ -2,13 +2,17 @@ from Player import player
 class edge():
     
     def __init__(self,fro=0,to=0,volume=0):
-        self.volume=volume
-        self.fro=fro
-        self.to=to
+        self._volume=volume
+        self._from=fro
+        self._to=to
         #player.__init__(self,fro,to)
     
-        
+    def get_from(self):
+        return self._from
+    def get_to(self):
+        return self._to
+    def get_volume(self):
+        return self._volume
     
-    def display(self):
-        print("the edge is from {} to {}".format(self.fro.num,self.to.num))
-        
+    def __str__(self):
+        return "from player no "+str(self._from._num)+" to player no  "+str(self._to._num)+" volume is  "+str(self._volume)
